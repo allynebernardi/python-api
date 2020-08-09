b8d80086a1015ee431c5193760fca0f7817a7c61
Aline Bernardi da Rocha <you@example.comallynebernardi@gmail.com>
2020-08-09 00:58:33 -0300

    teste web router

--- user.py
+++ user.py
@@ -1,12 +1,13 @@
 import os
 from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
 
 db_connect = create_engine('sqlite:///exemplo.db')
 app = Flask(__name__)
 api = Api(app)
-app = Flask(__name__)
+
 
 @app.route("/")
 def index():
